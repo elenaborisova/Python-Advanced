5856b257d53f2bd81d71cf28890096925026c392
Elena Borisova <elenaborrisova@gmail.com>
2020-11-08 19:46:08 +0200

    Update 05_truck_tour.py

--- 02. Lists as Stacks and Queues - Exercise/05_truck_tour.py
+++ 02. Lists as Stacks and Queues - Exercise/05_truck_tour.py
@@ -4,28 +4,26 @@ n = int(input())
 pumps = deque()
 
 for _ in range(n):
-    pump = list(map(int, input().split()))
-    pumps.append(pump)
+    petrol, distance = list(map(int, input().split()))
+    pumps.append([petrol, distance])
 
 
 original_pumps = pumps.copy()
-successful_pumps_sequence = []
-
-while len(successful_pumps_sequence) < n:
-    fuel = 0
-
-    for index, pump in enumerate(pumps):
-        fuel += pump[0]
-
-        if fuel < pump[1]:
-            for _ in range(index + 1):
-                pumps.append(pumps.popleft())
-
-            successful_pumps_sequence = []
-            break
-        else:
-            fuel -= pump[1]
-            successful_pumps_sequence.append(pump)
+additional_petrol = 0
+index = 0
 
 
-print(original_pumps.index(successful_pumps_sequence[0]))
+while index < len(pumps):
+    curr_pump = pumps[index]
+    petrol = pumps[index][0]
+    distance = pumps[index][1]
+
+    if petrol + additional_petrol >= distance:
+        additional_petrol += petrol - distance
+        index += 1
+    else:
+        pumps.append(pumps.popleft())
+        additional_petrol = 0
+        index = 0
+
+print(original_pumps.index(pumps[0]))
