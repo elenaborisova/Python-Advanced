e3c89ff0c11caaf576882be06c3f526da073f88b
Elena Borisova <elenaborrisova@gmail.com>
2020-12-08 18:45:29 +0200

    Update advent_calendar.py

--- 18. Exam Prep/advent_calendar.py
+++ 18. Exam Prep/advent_calendar.py
@@ -5,19 +5,6 @@ def fix_calendar(numbers):
                 numbers[i], numbers[j] = numbers[j], numbers[i]
     return numbers
 
-    # min_num = 10000
-    # sorted_count = 0
-    #
-    # while sorted_count < len(numbers):
-    #     for num in numbers[:len(numbers) - sorted_count]:
-    #         if num < min_num:
-    #             min_num = num
-    #     numbers.remove(min_num)
-    #     numbers.append(min_num)
-    #     min_num = 10000
-    #     sorted_count += 1
-    #
-    # return numbers
 
 numbers = [3, 2, 1, 7, 5, 9]
 fixed = fix_calendar(numbers)
